0221f1c431b5d83a112657a27f1093a97a83b91e
Zach Hamman <zhamman@gocurb.com>
2018-12-05 09:13:30 -0500

    restructure to flatten classes; no "new" for modules

--- lib/cloudwatchlogger.rb
+++ lib/cloudwatchlogger.rb
@@ -1,20 +1,14 @@
-require File.join(File.dirname(__FILE__), 'cloudwatchlogger', 'client')
-
+require 'cloudwatchlogger/client'
 require 'logger'
 
 module CloudWatchLogger
   class LogGroupNameRequired < ArgumentError; end
   class LogEventRejected < ArgumentError; end
 
-  def self.new(credentials, log_group_name, log_stream_name = nil, opts = {})
+  def self.setup_logger(log_group_name, log_stream_name = nil, credentials = {}, opts = {})
     client = CloudWatchLogger::Client.new(credentials, log_group_name, log_stream_name, opts)
     logger = Logger.new(client)
-
-    if client.respond_to?(:formatter)
-      logger.formatter = client.formatter
-    elsif client.respond_to?(:datetime_format)
-      logger.datetime_format = client.datetime_format
-    end
+    logger.formatter = client.formatter
 
     logger
   end
---
+++ lib/cloudwatchlogger/threaded.rb
@@ -0,0 +1,110 @@
+require 'aws-sdk-cloudwatchlogs'
+require 'thread'
+
+module CloudWatchLogger
+  class DeliveryThreadManager
+    def initialize(credentials, log_group_name, log_stream_name, opts = {})
+      @credentials = credentials
+      @log_group_name = log_group_name
+      @log_stream_name = log_stream_name
+      @opts = opts
+      start_thread
+    end
+
+    # Pushes a message to the delivery thread, starting one if necessary
+    def deliver(message)
+      start_thread unless @thread.alive?
+      @thread.deliver(message)
+      # Race condition? Sometimes we need to rescue this and start a new thread
+    rescue NoMethodError
+      @thread.kill # Try not to leak threads, should already be dead anyway
+      start_thread
+      retry
+    end
+
+    private
+
+    def start_thread
+      @thread = DeliveryThread.new(@credentials, @log_group_name, @log_stream_name, @opts)
+    end
+  end
+
+  class DeliveryThread < Thread
+    def initialize(credentials, log_group_name, log_stream_name, opts = {})
+      opts[:open_timeout] = opts[:open_timeout] || 120
+      opts[:read_timeout] = opts[:read_timeout] || 120
+      @credentials = credentials
+      @log_group_name = log_group_name
+      @log_stream_name = log_stream_name
+      @opts = opts
+
+      @queue = Queue.new
+      @exiting = false
+
+      super do
+        loop do
+          connect!(opts) if @client.nil?
+
+          msg = @queue.pop
+          break if msg == :__delivery_thread_exit_signal__
+
+          begin
+            event = {
+              log_group_name: @log_group_name,
+              log_stream_name: @log_stream_name,
+              log_events: [{
+                timestamp: (Time.now.utc.to_f.round(3) * 1000).to_i,
+                message: msg
+              }]
+            }
+            event[:sequence_token] = @sequence_token if @sequence_token
+            response = @client.put_log_events(event)
+            unless response.rejected_log_events_info.nil?
+              raise CloudWatchLogger::LogEventRejected
+            end
+            @sequence_token = response.next_sequence_token
+          rescue Aws::CloudWatchLogs::Errors::InvalidSequenceTokenException => err
+            @sequence_token = err.message.split(' ').last
+            retry
+          end
+        end
+      end
+
+      at_exit do
+        exit!
+        join
+      end
+    end
+
+    # Signals the queue that we're exiting
+    def exit!
+      @exiting = true
+      @queue.push :__delivery_thread_exit_signal__
+    end
+
+    # Pushes a message onto the internal queue
+    def deliver(message)
+      @queue.push(message)
+    end
+
+    def connect!(opts = {})
+      args = { http_open_timeout: opts[:open_timeout], http_read_timeout: opts[:read_timeout] }
+      args[:region] = @opts[:region] if @opts[:region]
+      args.merge( @credentials.key?(:access_key_id) ? { access_key_id: @credentials[:access_key_id], secret_access_key: @credentials[:secret_access_key] } : {} )
+
+      @client = Aws::CloudWatchLogs::Client.new(args)
+      begin
+        @client.create_log_stream(
+          log_group_name: @log_group_name,
+          log_stream_name: @log_stream_name
+          )
+      rescue Aws::CloudWatchLogs::Errors::ResourceNotFoundException
+        @client.create_log_group(
+          log_group_name: @log_group_name
+          )
+        retry
+      rescue Aws::CloudWatchLogs::Errors::ResourceAlreadyExistsException
+      end
+    end
+  end
+end
